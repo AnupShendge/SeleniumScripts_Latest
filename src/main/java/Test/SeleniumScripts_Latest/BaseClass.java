f976cc376f9ef3bf0f31320b40a3d4cba3bd3ef9
AnupShendge <shendgeanup@gmail.com>
2025-02-02 15:59:39 +0530

    first git commit for my selenium code

---
+++ src/main/java/Test/SeleniumScripts_Latest/BaseClass.java
@@ -0,0 +1,11 @@
+package Test.SeleniumScripts_Latest;
+ import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+	
+		
+	//launch the chrome
+    static WebDriver driver =new ChromeDriver();
+
+}
---
+++ src/main/java/Test/SeleniumScripts_Latest/LaunchBrowser.java
@@ -0,0 +1,18 @@
+package Test.SeleniumScripts_Latest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser extends BaseClass {
+	public static void main(String[] args) {
+		//System.setProperty("webdriver.chrome.driver",".\\driver\\chromedriver.exe");
+		//WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
+		String title=driver.getTitle();
+		System.out.println("Title of the webpage is " + title);
+		System.out.println("URL of the webpage is " + driver.getCurrentUrl());
+		//System.out.println(driver.getPageSource());
+		driver.close();
+	}
+
+}
